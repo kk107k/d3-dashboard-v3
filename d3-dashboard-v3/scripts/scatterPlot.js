c01d08ab25259e5a64113d01e71399c62cfc8167
K Kiasat <kk107@hw.ac.uk>
2024-04-01 13:38:50 +0400

    scatter plot done

--- d3-dashboard-v3/scripts/scatterPlot.js
+++ d3-dashboard-v3/scripts/scatterPlot.js
@@ -1,15 +1,15 @@
 // Set the dimensions and margins of the graph
 const margins = { top: 40, right: 20, bottom: 60, left: 80 },
-    widths = 960 - margin.left - margin.right,
-    heights = 500 - margin.top - margin.bottom;
+    widths = 500 - margins.left - margins.right,
+    heights = 350 - margins.top - margins.bottom;
 
 // Append the scattersvg object to the body of the page
 const scattersvg = d3.select("#scatterplot")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", widths + margins.left + margins.right)
+    .attr("height", heights + margins.top + margins.bottom)
     .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margins.left},${margins.top})`);
 
 // Define the div for the tooltip
 const tooltip = d3.select("body").append("div")
@@ -21,29 +21,36 @@ d3.csv("./data/world-data-2023-merged.csv").then(function (data) {
     // Add X axis
     const x = d3.scaleLinear()
         .domain([0, 50])
-        .range([0, width]);
+        .range([0, widths])
     scattersvg.append("g")
-        .attr("transform", `translate(0, ${height})`)
+        .attr("color","white")
+        .attr("transform", `translate(0, ${heights})`)
         .call(d3.axisBottom(x))
         .append("text")
         .attr("class", "axis-label")
         .attr("y", 50)
-        .attr("x", width / 2)
         .attr("text-anchor", "middle")
+        .style("font-size","15px")
+        .style("font-weight","100")
+        .style("fill","white")
         .text("Birth Rate");
 
     // Add Y axis
     const y = d3.scaleLinear()
         .domain([0, 90])
-        .range([height, 0]);
+        .range([heights, 0]);
     scattersvg.append("g")
         .call(d3.axisLeft(y))
+        .attr("color","white")
         .append("text")
         .attr("class", "axis-label")
         .attr("transform", "rotate(-90)")
         .attr("y", -50)
-        .attr("x", -height / 2)
+        .attr("x", -heights / 2)
         .attr("text-anchor", "middle")
+        .style("font-size","15px")
+        .style("font-weight","100")
+        .style("fill","white")
         .text("Life Expectancy");
 
     // Add dots
@@ -54,12 +61,15 @@ d3.csv("./data/world-data-2023-merged.csv").then(function (data) {
         .append("circle")
         .attr("cx", d => x(d['Birth Rate']))
         .attr("cy", d => y(d['Life expectancy']))
+        .attr("font-size", "30px")
+        .style("color","white")
         .attr("r", 5)
-        .style("fill", "#69b3a2")
+        .style("fill", "#BFF5FF")
+        .style("opacity", .4)
         .on("mouseover", function (event, d) {
             tooltip.transition()
                 .duration(200)
-                .style("opacity", .9);
+                .style("opacity", .9)
             tooltip.html("Country: " + d.Country + "<br/>Birth Rate: " + d['Birth Rate'] + "<br/>Life Expectancy: " + d['Life expectancy'])
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 15) + "px");
